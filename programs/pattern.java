1c9d54206eb34fde718abdf707271f3ec0851725
Amit <ak1454789@gmail.com>
2022-07-24 16:35:38 +0530

    new pattern

--- programs/pattern.java
+++ programs/pattern.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-class first{
+class pattern{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Number of rows");
